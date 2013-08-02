9d07143956e3b6a9dc12b61ca941945eb5e8f829
rmilovic <milovic.rade90@gmail.com>
2013-08-02 09:37:38 +0200

    Adds new service called ViewDataService used for data retreival

---
+++ Execom.IOG/Services/Data/ViewDataService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Execom.IOG.Graph;
+
+namespace Execom.IOG.Services.Data
+{
+    internal class ViewDataService
+    {
+        private INodeProvider<Guid, object, EdgeData> nodes;
+        private TypesService typesService;
+
+        public ViewDataService(INodeProvider<Guid, object, EdgeData> nodes, TypesService typesService)
+        {
+            this.nodes = nodes;
+            this.typesService = typesService;
+        }
+    }
+}
